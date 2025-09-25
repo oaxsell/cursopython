36ff0659eb51a141580d6417b80e268b395bb876
Axsell Luis Eker de Souza <axsell27375116@edu.df.senac.br>
2025-09-24 21:46:38 -0300

    Atualizao do comentario inserindo roteiro de venv

---
+++ aula01/dev/desafio.py
@@ -0,0 +1,13 @@
+"""
+Para rodar esse programa faz-se necessário criar e ativar um ambiente virtual.
+Para criar o ambiente virtual, utilize o comando:
+python -m venv venv
+
+Para ativar o ambiente virtual, utilize o comando:
+No Windows: venv\Scripts\activate
+No git bash: source venv/Scripts/activate
+"""
+# Comentário de uma linha
+
+# Saida de dados
+print("Hello, World!")
