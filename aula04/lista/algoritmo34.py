89fbd547556496606ad48d635297bcdcaa8465b4
Axsell Eker <axsell@live.com>
2025-10-02 19:52:16 -0300

    Atualizacao codigo 35

--- aula04/lista/algoritmo34.py
+++ aula04/lista/algoritmo34.py
@@ -1,9 +1,9 @@
 """
 Algoritmo 34: Ler um número inteiro e imprimir seu sucessor e seu antecessor.
 """
-numero_inteiro = int(input("Digite o primeiro número inteiro: "))
+numero_inteiro = int(input("Digite um número inteiro: "))
 numero_antecessor = numero_inteiro - 1
 numero_sucessor = numero_inteiro + 1
 
 
-print(f"Numero inteiro 1: {numero_inteiro} e Numero Sucessor: {numero_sucessor} e Numero Antecessor: {numero_antecessor}")
+print(f"Numero inteiro digitado: {numero_inteiro} e Numero Sucessor: {numero_sucessor} e Numero Antecessor: {numero_antecessor}")
